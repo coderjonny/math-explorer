f3706b41b13c1387e844d6e5291b7b1d39b7fcd5
Jonny Kang <jonny@bypass.la>
2025-03-20 17:23:38 -0700

    adding equation and vizualization on the graph

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,47 +1,124 @@
-import { StyleSheet, SafeAreaView, Dimensions } from "react-native";
-import { Canvas, Rect } from "@shopify/react-native-skia";
+import {
+  StyleSheet,
+  SafeAreaView,
+  Dimensions,
+  ActivityIndicator,
+  View,
+  Text,
+} from "react-native";
+import { Canvas, Group, Rect } from "@shopify/react-native-skia";
+import { useEffect, useState } from "react";
 
-const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
-const COLS = 100;
-const ROWs = 100;
-const squareWidth = SCREEN_WIDTH / COLS;
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
+const COLS = 200;
+const ROWS = 200;
+const squareWidth = SCREEN_WIDTH / ROWS;
+
+type square = {
+  row: number;
+  col: number;
+};
 
 export default function HomeScreen() {
+  const [squares, setSquares] = useState<square[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const isBounded = (x: number, y: number) => {
+    const maxIterations = 40;
+    let realNum = 0;
+    let imaginaryNum = 0;
+
+    for (let i = 0; i < maxIterations; i++) {
+      const realNumSquared = realNum * realNum;
+      const imaginaryNumSquared = imaginaryNum * imaginaryNum;
+      const complexNumber = realNum * realNum + imaginaryNum * imaginaryNum;
+
+      // console.log({ i, x, y, realNum, imaginaryNum, complexNumber });
+      if (complexNumber > 4) return false;
+
+      const nextImaginaryNum = 2 * realNum * imaginaryNum + y;
+      realNum = realNumSquared - imaginaryNumSquared + x;
+      imaginaryNum = nextImaginaryNum;
+    }
+
+    return true;
+  };
+
+  const getCoordinates = (row: number, col: number) => {
+    const minX = -2;
+    const maxX = 2;
+    const minY = -2;
+    const maxY = 2;
+    const gridWidth = maxX - minX;
+    const gridHeight = maxY - minY;
+    const x = gridWidth * (row / ROWS) - maxX;
+    const y = gridHeight * (col / COLS) - maxY;
+    return [x, y];
+  };
+
+  useEffect(() => {
+    let squares: square[] = [];
+    for (let row = 0; row < ROWS; row++) {
+      for (let col = 0; col < COLS; col++) {
+        const [xComponent, yComponent] = getCoordinates(row, col);
+        const square = { row, col };
+        if (isBounded(xComponent, yComponent)) {
+          squares.push(square);
+        }
+      }
+    }
+    setSquares(squares);
+    setLoading(false);
+  }, []);
+
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Canvas style={{ flex: 1 }}>
-        {Array.from({ length: COLS }).map((_, col) =>
-          Array.from({ length: ROWs }).map((_, row) => (
-            <Rect
-              key={`${col}-${row}`}
-              x={col * squareWidth}
-              y={row * squareWidth}
-              width={squareWidth}
-              height={squareWidth}
-              color={`hsl(${(col / COLS) * 360}, 100%, 50%)`}
-            />
-          ))
+    <SafeAreaView style={{ flex: 1, alignItems: "flex-start" }}>
+      <View style={{ flexDirection: "row", justifyContent: "center" }}>
+        {loading ? (
+          <View style={styles.loading}>
+            <ActivityIndicator size="large" color="#0000ff" />
+          </View>
+        ) : (
+          <Canvas style={styles.canvas}>
+            <Group>
+              {squares.map((square, index) => (
+                <Rect
+                  key={index}
+                  x={square.row * squareWidth}
+                  y={square.col * squareWidth}
+                  width={squareWidth}
+                  height={squareWidth}
+                  color={`hsl(${(square.row / COLS) * 360}, 100%, 50%)`}
+                />
+              ))}
+            </Group>
+          </Canvas>
         )}
-      </Canvas>
+      </View>
+      <View style={styles.titleContainer}>
+        <Text>
+          {ROWS * COLS} calcuations 
+        </Text>
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  loading: {
+    flex: 1,
+    justifyContent: "center",
+    marginTop: SCREEN_WIDTH / 2,
+  },
+  canvas: {
+    flex: 1,
+    backgroundColor: "darkblue",
+    height: SCREEN_WIDTH,
+    alignSelf: "center",
+  },
   titleContainer: {
-    flexDirection: "row",
+    flex: 1,
     alignItems: "center",
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
+    padding: 10,
   },
 });
